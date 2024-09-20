a192f418a935d71a82069bac89383c3815204021
Lucas Mattos <lucasmattosdev@gmail.com>
2024-09-20 18:18:24 -0300

    chore: initial project

---
+++ src/main/java/dev/lucasmattos/backend/JavaBackendBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package dev.lucasmattos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendBoilerplateApplication.class, args);
+    }
+
+}
